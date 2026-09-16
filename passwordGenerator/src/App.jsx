ef9396b87ff63d240842d8013b41cc914c883f60
Hifza Naseem <hifzaanaseem78@gmail.com>
2026-09-16 10:05:07 +0500

    basic structure

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -1,13 +1,47 @@
-import { useState } from 'react'
+import { useState, useCallback } from 'react'
 
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [length, setLength] = useState(8)
+  const [numAllowed, setNumAllowed] = useState(false)
+  const [charAllowed, setCharAllowed] = useState(false)
+  const [password, setPassword] = useState("")
+ 
+  const passwordGenerator = (() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (numAllowed) {
+      str += "0123456789";
+    }
+    if (charAllowed) {
+      str += "`!@#$%^&*()_+{}[]';:.,<>|";
+    }
+    
+    for (let i=1; i<=array.length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass = str.indexAt(char);
+    }
+
+    setPassword(pass);
+
+  }, [length, numAllowed, charAllowed, setPassword])
 
   return (
     <>
       
+      <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-700'>
+        <h1 className='text-white text-center my-3'>Password Generator</h1>
+        <div className='flex shadow rounded-lg overflow-hidden mb-4'>
+          <input 
+          type='text'
+          value={password}
+          className='outline-none w-full py-1 px-3'
+          placeholder='password'
+          readOnly
+          />
+          </div>
+          </div>
     </>
   )
 }
