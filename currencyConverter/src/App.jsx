28ff0f66cb7b9bb1494b6aa7969a88b5a2ed565b
Hifza Naseem <hifzaanaseem78@gmail.com>
2026-09-19 13:12:49 +0500

    started new project revision

---
+++ currencyConverter/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1 className='bg-orange-300 text-3xl text-center p-3'>Currency Converter</h1>
+
+    </>
+  )
+}
+
+export default App
